3adadcbab22b4925cedbb03bb0392b4f39d91fc6
astrid hernandez <53374666+julieth132412@users.noreply.github.com>
2020-10-07 00:28:27 -0500

    Add files via upload

---
+++ adsi/includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'bsei26xjivpbkksbgjsi-mysql.services.clever-cloud.com' );          // Set database host
+  define( 'DB_USER', 'uwmpvcc6cxce6qjj' );             // Set database user
+  define( 'DB_PASS', 'uFsDoBxd0XDuN2fElCQ5' );             // Set database password
+  define( 'DB_NAME', 'bsei26xjivpbkksbgjsi' );        // Set database name
+
+?>
